cc707c5a491f30b6864c8522eff96882bf1a514d
Pavel Kozhushko <81619884+pakodev28@users.noreply.github.com>
2022-12-15 12:29:30 +0300

    add 4 and 5 fixes

--- fixes.py
+++ fixes.py
@@ -3,6 +3,7 @@ import re
 
 
 def to_camel_case(text):
+
     return re.split("_|-| ", text)[0] + "".join(
         word.title() for word in re.split("_|-| ", text)[1::]
     )
@@ -20,3 +21,13 @@ class SingletonMeta(type):
       
 # 3
 count_bits = lambda n: bin(n).count("1")
+
+# 4
+
+def digital_root(n):
+
+    return n if n < 10 else digital_root(sum(map(int, str(n))))
+
+# 5
+
+even_or_odd = lambda number: "Even" if number % 2 == 0 else "Odd"
